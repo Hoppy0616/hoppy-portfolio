a8ec06aea4d38142ca8ecfee7f8add4e9da8557a
Hoppy0616 <hodahoda.20430@gmail.com>
2021-07-12 16:26:46 +0900

    ページの追加

---
+++ src/pages/hobby.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+const HobbyPage = () => {
+
+}
+export default HobbyPage
